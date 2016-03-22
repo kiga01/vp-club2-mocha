05471b3c35e914ad86bc2f2c325347ce02b9c341
kiga01 <ahinestrosa1984@gmail.com>
2016-03-22 15:15:05 -0500

    gulp task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+var testDirectory = [
+    'tests/*.js'
+];
+
+gulp.task('test', () =>{
+    return gulp.src(testDirectory, {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
+});
